121d8c79c196ae73abe9d7049cd5883be539a4d5
Christian Vaas <chrisvaas@gmail.com>
2018-10-02 18:35:02 +0200

    Added configurable users

--- README.md
+++ README.md
@@ -4,5 +4,17 @@
 
 This plugin expects the usernames used for authentication to be the email address they are authorized to send emails from. For example `user1@domain.com` can only send emails from his own address but not `user2@domain.com`.
 
+Configuration
+-------------
+
+Configuration is stored in `config/access_auth.ini` and uses the INI style formatting. The following example allows a user authenticated as `user1` to send emails from `user1@domain.com` and `support@domain.com`. If the username used for authentication is an email address, e.g., `user@domain.com`, that user can send from his own email address.
+
+Example:
+
+```
+[acl]
+user1=user1@domain.com,support@domain.com
+```
+
 [npm-img]: https://nodei.co/npm/haraka-plugin-access-auth.png
 [npm-url]: https://www.npmjs.com/package/haraka-plugin-access-auth
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+exports.register = function () {
+  this.load_access_auth_ini();
+}
+
+exports.load_access_auth_ini = function () {
+  const plugin = this;
+  plugin.cfg = plugin.config.get('acces_auth.ini', function () {
+    plugin.load_access_auth_ini();
+  });
+  plugin.cfg.acl = plugin.cfg.acl || {}
+}
+
 exports.hook_mail = function (next, connection, params) {
+  const plugin = this;
   const mail_from = params[0];
-  if (connection.notes.auth_user !== mail_from.address()) {
+  const auth_user = connection.notes.auth_user
+  const authorized = (plugin.cfg.acl[auth_user] || '').split(',')
+    .map(itm => itm.trim())
+    .reduce((acc, itm) => acc || itm === mail_from.address(), auth_user === mail_from.address())
+
+  if (!authorized) {
     return next(DENY, 'You are not authorized to send from this address');
   }
   return next();
