09ee3e7682894dae5250647d7429dedc96738bef
vaas <auspex@auspex.eu>
2019-04-25 20:25:03 +0800

    Ensure the full regex is matched

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ exports.hook_mail = function (next, connection, params) {
   const auth_user = connection.notes.auth_user
   const authorized = (this.cfg.acl[auth_user] || '').split(',')
     .map(itm => itm.trim())
-    .reduce((acc, itm) => acc || mail_from.address().match(new RegExp(itm)), auth_user === mail_from.address())
+    .reduce((acc, itm) => acc || mail_from.address().match(new RegExp(`^${itm}$`)), auth_user === mail_from.address())
 
   if (!authorized) {
     return next(DENY, 'You are not authorized to send from this address');
